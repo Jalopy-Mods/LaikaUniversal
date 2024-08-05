8a8d9b137bae1771fba9bea1c045a983bef0cc0e
Meb do stuff <69876322+Meb-Do-Stuff@users.noreply.github.com>
2024-08-05 02:31:08 +0200

    Added stuff or whatever Have to figure out how to have the boot opening properly.

--- LaikaUniversal/Mod.cs
+++ LaikaUniversal/Mod.cs
@@ -10,8 +10,8 @@ namespace Universal
         public override string ModAuthor => "itisgt & Meb";
         public override string ModDescription => "Turn the Trabby into a Laika Universal";
         public override string ModVersion => "1.0.0";
-        public override string GitHubLink => "";
-        public override WhenToInit WhenToInit => WhenToInit.InMenu;
+        // public override string GitHubLink => "https://github.com/Jalopy-Mods/LaikaUniversal";
+        public override WhenToInit WhenToInit => WhenToInit.InGame;
 
         // public override List<(string, string, string)> Dependencies => new List<(string, string, string)>();
 
@@ -20,7 +20,11 @@ namespace Universal
         public override void OnEnable()
         {
             base.OnEnable();
+            LoadInGame();
+        }
 
+        private void LoadInMainMenu()
+        {
             var car = GameObject.Find("FrameHolder/TweenHolder/Frame");
             var assetBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/universal");
             if (assetBundle == null)
@@ -60,10 +64,62 @@ namespace Universal
                         break;
                 }
             }
+            var seat = Instantiate(assetBundle.LoadAsset<GameObject>("seat"), parent: car.transform.Find("Plane_1862"));
+            seat.transform.localPosition = new Vector3(0, 0, 0);
+            assetBundle.Unload(false);
+        }
+
+        public void LoadInGame()
+        {
+            Debug.LogWarning("Started");
+            var car = GameObject.Find("FrameHolder/TweenHolder/Frame");
+            var assetBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/universal");
+            if (assetBundle == null)
+            {
+                Debug.LogError("Failed to load Asset Bundle");
+                return;
+            }
+
+            var universal = Instantiate(assetBundle.LoadAsset<GameObject>("LaikaUniversal"), parent: car.transform);
+            Destroy(car.GetComponent<MeshFilter>());
+            Destroy(car.transform.Find("Roof").GetComponent<MeshFilter>());
+            Destroy(car.transform.GetChild(car.transform.childCount - 3).gameObject);
+            Destroy(car.transform.Find("rearWindow").GetComponent<MeshFilter>());
+            Destroy(car.transform.Find("Plane_1862").GetComponent<MeshFilter>());
+            Destroy(car.transform.Find("RL_Window").GetComponent<MeshFilter>());
+            Destroy(car.transform.Find("RR_Window").GetComponent<MeshFilter>());
+
+            universal.transform.localPosition = new Vector3(0, -4.35f, 0);
+
+            foreach (var obj in FindObjectsOfType<GameObject>())
+            {
+                if (obj.name != "Boot" || obj.transform.childCount == 0) continue;
+                switch (obj.transform.GetChild(0).name)
+                {
+                    case "Boot":
+                        Destroy(obj.GetComponent<MeshFilter>());
+                        universal.transform.GetChild(0).parent = obj.transform;
+                        car.transform.Find("R_LicensePlate").localPosition = new Vector3(-5.65f, -3.55f, 0);
+                        break;
+                    case "BootLock":
+                        obj.transform.localPosition = new Vector3(-0.07f, -0.525f, -0.3f);
+                        Destroy(obj.transform.GetChild(0).GetComponent<MeshFilter>());
+                        obj.transform.GetChild(0).GetChild(0).localScale = new Vector3(0.6f, 0.6f, 0.6f);
+                        break;
+                    case "Slot 1":
+                        Destroy(obj.GetComponent<MeshFilter>());
+                        Destroy(obj);
+                        Debug.LogWarning("Destroyed Boot Slot 1");
+                        break;
+                }
+            }
 
             var seat = Instantiate(assetBundle.LoadAsset<GameObject>("seat"), parent: car.transform.Find("Plane_1862"));
             seat.transform.localPosition = new Vector3(0, 0, 0);
             assetBundle.Unload(false);
+
+            // Rotation not managed by mesh origin, but by script iTween and DoorLogicC
+            // Can maybe create a harmony prefix to change the position of the door while the true component is doing its thing in the meantime.
         }
     }
 }
